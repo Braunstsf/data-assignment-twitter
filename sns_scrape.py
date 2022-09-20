5e29d215c1d38dd36948df382dcfb6d3c642b755
Braunstsf <113804405+Braunstsf@users.noreply.github.com>
2022-09-19 23:16:24 -0500

    Add files via upload

---
+++ sns_scrape.py
@@ -0,0 +1,16 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+query = "iPhone 14 until:2022-09-19 since:2022-09-06"
+tweets = []
+limit = 100
+for tweet in sntwitter.TwitterSearchScraper(query).get_items(): 
+	if len(tweets) == limit:
+		break
+	else: 
+		tweets.append([tweet.date,tweet.user.username, tweet.content, tweet.sourceUrl, tweet.lang, tweet.coordinates, tweet.user.location, tweet.likeCount, tweet.user.friendsCount, tweet.replyCount, tweet.retweetCount])
+
+	#print(vars(tweet))
+	#break
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet', 'sourceUrl', 'Language', 'Coordinates', 'Location', 'Like Count', 'Friends', 'Replies', 'Retweets'])
+print(df)
