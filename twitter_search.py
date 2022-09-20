d808eb2eb1889334d12728afba6b359578db1c74
Braunstsf <113804405+Braunstsf@users.noreply.github.com>
2022-09-20 01:28:29 -0500

    Update twitter_search.py

--- twitter_search.py
+++ twitter_search.py
@@ -5,11 +5,9 @@ import tweepy_authentication
 #getting the credentials from a .env file in my repository on Github: https://github.com/Braunstsf/hcd-data-assignment
 client = tweepy.Client(bearer_token=tweepy_authentication.bearer_token)
 query = 'iPhone OR iPhone 14' 
-output = client.search_all_tweets(query=query, max_results=10, tweet_fields=['created_at', 'lang', 'geo'], expansions=['author_id'])
+output = client.search_all_tweets(query=query, max_results=10, tweet_fields=['created_at', 'lang'], expansions=['author_id'])
 
 users = {u['id']: u for u in output.includes['users']}
-
-data.public_metrics.retweet_count
 
 for tweet in output.data:
   if users[tweet.author_id]: 
@@ -19,7 +17,6 @@ for tweet in output.data:
     # print(tweet.created_at)
      print(tweet.lang)
     # print(tweet.text)
-     print(tweet.description)
    # print(tweet.lang)
     #print(tweet.created_at)
    
